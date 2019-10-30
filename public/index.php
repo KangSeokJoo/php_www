96bda2fb142b5f5635af6e66e9c4861e6d36e0bf
Sseokkoo <kangsukjoo1@naver.com>
2019-10-30 11:21:13 +0900

    데이터 베이스 접속 및 테이블

--- public/index.php
+++ public/index.php
@@ -4,8 +4,8 @@ $config = include "../dbconf.php";
 echo "대림대학교";
 print_r($config);
 
-require "../Module/Database/Database.php";
-require "../Module/Database/Table.php";
+require "../Module/Database/database.php"; // 1개
+require "../Module/Database/table.php"; //2개
 
 $db = new Database($config);
 
