f8de5ef5e98d5d9c99a3b3e879558d26f8ac3dec
Sseokkoo <kangsukjoo1@naver.com>
2019-11-27 10:12:59 +0900

    왜 저번주 깃 에드 안됐지

--- App/Controller/Databases.php
+++ App/Controller/Databases.php
@@ -1,22 +1,43 @@
 <?php
 namespace App\Controller;
-
 class Databases
 {
     private $db;
-    //생성자
+    // 생성자
     public function __construct($db)
     {
-        $this->db =$db;
+        // echo __CLASS__;
+        $this->db = $db; 
     }
-
     public function main()
     {
-        
+        $uri = new \Module\Http\Uri;
+        if($uri->second() == "new") {
+            print_r($_POST);
+            if($_POST) {
+                // 새로운 데이터베이스를 추가
+                $query = "CREATE DATABASE ".$_POST['database'];
+                echo $query;
+                $result = $this->db->queryExecute($query);
+                // 페이지 이동
+                header("location:"."/databases");
+            } else {
+                // 데이터베이스 입력해 주세요
+                // 새로운 데이터베이스 추가
+                // echo "데이터베이스를 생성해 주세요.";
+                $htmlForm = file_get_contents("../Resource/database_new.html");
+                echo $htmlForm;
+            }
+            
+        } else {
+            // 데이터베이스 목록
+            $this->list();
+        }        
+    }
+    public function list()
+    {
         $html = new \Module\Html\HtmlTable;
-
-        // echo "<br>";
-        $query = "SHOW Databases";
+        $query = "SHOW DATABASES";
         $result = $this->db->queryExecute($query);
         $count = mysqli_num_rows($result);
         $content = ""; // 초기화
@@ -24,15 +45,14 @@ class Databases
         for ($i=0;$i<$count;$i++) {
             $row = mysqli_fetch_object($result);
             // $rows []= $row; // 배열 추가
-            // 배열의 배열 추가 = 2차원 배열
-            // 키 , 값  연상배열
-            $rows [] = [
-                'Key 값' => $i ,
-                '테이블명' => "<a href='/Tables/".$row->Database."'>".$row->Database."</a>"
+            // 배열 * 배열 = 2차원 배열.
+            // 키, 값 연상배열
+            $rows []= [
+                'num'=>$i,
+                'name'=>"<a href='/Tables/".$row->Database."'>".$row->Database."</a>"
             ];
         }
         $content = $html->table($rows);
-
         $body = file_get_contents("../Resource/database.html");
         $body = str_replace("{{content}}",$content, $body); // 데이터 치환
         echo $body;
--- public/index.php
+++ public/index.php
@@ -1,28 +1,33 @@
 <?php
-
 $config = include "../dbconf.php";
-
 require "../Loading.php";
-/*
-require "../Module/Database/database.php"; // 1개
-require "../Module/Database/table.php"; // 2개
-*/
-
+// 세션 활성화
+session_start();
 $uri = $_SERVER['REQUEST_URI'];
-$uris = explode("/", $uri); //uri의 문자열을 '/' 기준으로 짤라낼꺼다 .
-
-$db = new \Module\Database\Database($config);
-
-if(isset($uris[1]) && $uris[1]){ // 배열이 있냐 확인, 배열값이 있냐없냐 확인
-    // echo $uris[1]."컨트롤러 실행 ...";
+$uris = explode("/",$uri); // 파란책
+// print_r($uris);
+$db = new \Module\Database\Database( $config );
+if(isset($uris[1]) && $uris[1]) {
+    // 컨트롤러 실행...
+    // echo $uris[1]."컨트롤러 실행...";
     $controllerName = "\App\Controller\\" . ucfirst($uris[1]);
     $tables = new $controllerName ($db);
+    
+    // 클래스의 메인이 처음으로 동작하는 것로 정해요.
     $tables->main();
-}else{
-    // echo "처음 페이지 에요";
+} else {
+    // 처음 페이지 에요.
+    // echo "처음 페이지 에요.";
     $body = file_get_contents("../Resource/index.html");
+    if($_SESSION["email"]) {
+        // 로그 상태 입니다.
+        $body = str_replace("{{Login}}","로그인 상태입니다. <a href='logout'>로그아웃</a>",$body);
+    } else {
+        // 로그인 해주세요.
+        $loginForm = file_get_contents("../Resource/login.html");
+        $body = str_replace("{{Login}}",$loginForm,$body);
+    }
     echo $body;
 }
-
 // $desc = new \App\Controller\TableInfo;
 // $desc->main();
